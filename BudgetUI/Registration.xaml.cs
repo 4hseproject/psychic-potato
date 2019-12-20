edc8f9143a784f5f5faea3f201b3661e12f63e51
Мария Андреева <andreeva.maria.s@gmail.com>
2019-12-20 14:33:04 +0300

    безуспешные поптытки сделать не баг, а фичу

--- BudgetUI/Registration.xaml.cs
+++ BudgetUI/Registration.xaml.cs
@@ -23,6 +23,7 @@ namespace BudgetUI
         public Registration()
         {
             InitializeComponent();
+            TextBox_Answer.Text = "Answer";
             
         }
 
@@ -60,8 +61,20 @@ namespace BudgetUI
 
         private void TextBox_name_TextChanged(object sender, TextChangedEventArgs e)
         {
-            TextBox_name.Text = "";
 
+            TextBox_Answer.Text = "";
+
+        }
+
+        bool hasBeenClicked = false;
+        private void TextBox_Answer_Focus(object sender, RoutedEventArgs e)
+        {
+            if (!hasBeenClicked)
+            {
+                TextBox box = sender as TextBox;
+                box.Text = String.Empty;
+                hasBeenClicked = true;
+            }
         }
     }
 }
