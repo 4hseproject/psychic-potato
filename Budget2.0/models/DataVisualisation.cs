41261f35dea9b73ad44909c17edfc3e3bb99a018
Алишер Шамсиев <akhshamsiev@edu.hse.ru>
2019-12-20 17:40:35 +0300

    lopata

---
+++ Budget2.0/models/DataVisualisation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budget2._0.models
+{
+    public class DataVisualisation
+    {
+        public string Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string IncomeOrSpending{ get; set; }
+        public string CategoryName { get; set; }
+    }
+}
